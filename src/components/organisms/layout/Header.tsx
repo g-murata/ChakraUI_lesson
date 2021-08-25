3ad4f9210dda6a3671b8720abd46fc5f397e8e74
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2021-08-25 20:53:41 +0900

    69.ヘッダーの作成

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,25 @@
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
 
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useHistory } from "react-router-dom";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const history = useHistory();
+
+  const onClickHome = useCallback(() => history.push("/home"), [history]);
+  const onClickUserManagement = useCallback(
+    () => history.push("/home/user_management"),
+
+    [history]
+  );
+  const onClickSetting = useCallback(() => history.push("/home/setting"), [
+    history
+  ]);
+
   return (
     <>
       <Flex
@@ -16,25 +30,39 @@ export const Header: VFC = memo(() => {
         justify="space-between"
         padding={{ base: 3, md: 5 }}
       >
-        <Flex align="center" as="a" mr={8} _hover={{ cursol: "pointer" }}>
+        <Flex
+          align="center"
+          as="a"
+          mr={8}
+          _hover={{ cursol: "pointer" }}
+          onClick={onClickHome}
+        >
           <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
             ユーザー管理アプリ
           </Heading>
         </Flex>
+
         <Flex
           align="center"
-          fontsize="sm"
+          fontSize="sm"
           flexGrow={2}
           display={{ base: "none", md: "flex" }}
         >
           <Box pr={4}>
-            <Link>ユーザ一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザ一覧</Link>
           </Box>
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 });
